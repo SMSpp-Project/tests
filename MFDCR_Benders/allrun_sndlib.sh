af904920881a6dc4187f15288ad6bc553c322304
dmeoli <donato.meoli.95@gmail.com>
2026-06-24 12:15:55 +0200

    Revert "New MFDCR_Benders tester"
    This reverts commit 40d0b7249cd13d9455660fc8a58d8f6bb08aab34.

--- MFDCR_Benders/allrun_sndlib.sh
+++
@@ -1,6 +0,0 @@
-for f in abilene atlanta cost266 dfn-bwin dfn-gwin di-yuan france geant india35 newyork nobel-eu nobel-germany nobel-us pdh pioro40 polska ta2
-do
-./MFDCRBlock_test ../sndlib/$f.nod #>> $f.out
-done
-
-#abilene atlanta cost266 dfn-bwin dfn-gwin di-yuan france geant germany50 giul39 india35 janos-us-ca janos-us newyork nobel-eu nobel-germany nobel-us norway pdh pioro40 polska sun ta2
